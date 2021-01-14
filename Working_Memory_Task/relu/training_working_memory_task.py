5383c96b2a19f819e3cb5c1c32adf74e4e57d240
Amrut Nadgir <31873854+amrutn@users.noreply.github.com>
2021-01-14 15:07:07 -0800

    increased num_trials to 25

--- Working_Memory_Task/relu/training_working_memory_task.py
+++ Working_Memory_Task/relu/training_working_memory_task.py
@@ -107,7 +107,7 @@ for iter in tqdm(range(num_iters * 5), leave = True, position = 0):
     inputs.append(network.convert(time, input_funcs))
     error_masks.append(network.convert(time, [error_mask_func]))
 print('Training...', flush = True)
-weight_history, losses = network.train(num_iters, targets, time, num_trials = 10, inputs = inputs,
+weight_history, losses = network.train(num_iters, targets, time, num_trials = 25, inputs = inputs,
               input_weight_matrix = input_weight_matrix, learning_rate = .0005, error_mask = error_masks, save = 1)
 
 net_weight_history['trained weights'] = np.asarray(weight_history).tolist()
