9b30f2d3b1bf007298127c9cc09e6c0643c7deb9
Amrut Nadgir <31873854+amrutn@users.noreply.github.com>
2021-01-15 12:03:07 -0800

    small mistake

--- Working_Memory_Task/relu/training_working_memory_task.py
+++ Working_Memory_Task/relu/training_working_memory_task.py
@@ -106,7 +106,7 @@ for iter in tqdm(range(num_iters * 50), leave = True, position = 0):
     error_masks.append(network.convert(time, [error_mask_func]))
 print('Training...', flush = True)
 weight_history, losses = network.train(num_iters, targets, time, num_trials = 50, inputs = inputs,
-              input_weight_matrix = input_weight_matrix, learning_rate = .001, error_mask = error_masks, save = 1)
+              input_weight_matrix = input_weight_matrix, learning_rate = .001, epochs=50, error_mask = error_masks, save = 1)
 
 net_weight_history['trained weights'] = np.asarray(weight_history).tolist()
 
