6956d94980a8b6591275d76e73ed494c736e3018
Amrut Nadgir <31873854+amrutn@users.noreply.github.com>
2021-01-16 10:38:37 -0800

    changed working memory task to two inputs

--- Working_Memory_Task/relu/training_working_memory_task.py
+++ Working_Memory_Task/relu/training_working_memory_task.py
@@ -13,9 +13,9 @@ num_iters = int(input("Enter number of training iterations: "))
 num_nodes = int(input("Enter number of nodes: "))
 #Defining Network
 time_constant = 100 #ms
-timestep = 5 #ms
+timestep = 10 #ms
 noise_strength = .01
-num_inputs = 3#4, for prompt
+num_inputs = 4
 
 connectivity_matrix = np.ones((num_nodes, num_nodes))
 weight_matrix = np.random.normal(0, 1/np.sqrt(num_nodes), (num_nodes, num_nodes))
@@ -27,27 +27,26 @@ connectivity_matrix = tf.constant(connectivity_matrix)
 
 noise_weights = 1 * np.ones(num_nodes)
 bias_weights = np.random.normal(0, 1/np.sqrt(num_inputs), num_nodes)
-input_weights = np.random.normal(0, 1/np.sqrt(num_inputs), num_nodes)/2
-#prompt_weights = np.random.normal(0, 1/np.sqrt(num_inputs), num_nodes)/2
+input1_weights = np.random.normal(0, 1/np.sqrt(num_inputs), num_nodes)/2
+input2_weights = np.random.normal(0, 1/np.sqrt(num_inputs), num_nodes)/2
 
-input_weight_matrix = tf.constant(np.vstack((bias_weights, noise_weights, input_weights)))#prompt_weights
+input_weight_matrix = tf.constant(np.vstack((bias_weights, noise_weights, input1_weights, input2_weights)))
 
-def rule_input(time):
-    #No input for now
-    return 0
-'''
-def prompt(time):
+def input1(time):
     #No input for now
     return 0
 
-'''    
+def input2(time):
+    #No input for now
+    return 0
+    
 def bias(time):
     return 1
 def noise(time):
     return np.sqrt(2 * time_constant/timestep) * noise_strength * np.random.normal(0, 1)
 
 
-input_funcs = [bias, noise, rule_input]#, prompt]
+input_funcs = [bias, noise, input1, input2]
 
 init_activations = tf.constant(np.zeros((num_nodes, 1)))
 output_weight_matrix = tf.constant(np.random.uniform(0, 1/np.sqrt(num_nodes), (1, num_nodes)))
@@ -61,25 +60,17 @@ time = 10000
 def gen_functions():
     wait_time = int(np.random.uniform(2000, 3000))
     chosen_vals = np.random.uniform(0, 2, 2)
-    def rule_input(time):
-        #running for 15 seconds = 15000ms
-        if time < 1000:
-            return np.random.normal(0, .01)
-        elif time >= 1000 and time < 2000:
+    def input1(time):
+        if time >= 1000 and time < 2000:
             return chosen_vals[0] + np.random.normal(0, .01)
-        elif time >= 2000 and time < 2000 + wait_time:
+        else:
         	return np.random.normal(0, .01)
-        elif time >= 2000 + wait_time and time < 3000 + wait_time:
+    def input2(time):
+        if time >= 2000 + wait_time and time < 3000 + wait_time:
         	return chosen_vals[1] + np.random.normal(0, .01)
         else:
         	return np.random.normal(0, .01)
-    '''
-    def prompt(time):
-    	if time < 3000 + wait_time:
-    		return 0
-    	else:
-    		return 1
-	'''
+
     def target_func(time):
         #running for 15 seconds = 15000ms
         if time < 3000 + wait_time:
@@ -94,16 +85,16 @@ def gen_functions():
             return 0
         else:
             return 1
-    return rule_input, target_func, error_mask_func #prompt
+    return input1, input2, target_func, error_mask_func
 
 targets = []
 inputs = []
 error_masks = []
 print('Preprocessing...', flush = True)
-for iter in tqdm(range(num_iters * 50), leave = True, position = 0):
-    rule_input, target_func, error_mask_func = gen_functions() #prompt
-    input_funcs[2] = rule_input
-    #input_funcs[3] = prompt
+for iter in tqdm(range(num_iters * 10), leave = True, position = 0):
+    input1, input2, target_func, error_mask_func = gen_functions()
+    input_funcs[2] = input1
+    input_funcs[3] = input2
     targets.append(network.convert(time, [target_func]))
     inputs.append(network.convert(time, input_funcs))
     error_masks.append(network.convert(time, [error_mask_func]))
@@ -115,8 +106,8 @@ net_weight_history['trained weights'] = np.asarray(weight_history).tolist()
 
 net_weight_history['bias'] = bias_weights.tolist()
 net_weight_history['noise weights'] = noise_weights.tolist()
-net_weight_history['input weights'] = input_weights.tolist()
-#net_weight_history['prompt weights'] = prompt_weights.tolist()
+net_weight_history['input1 weights'] = input1_weights.tolist()
+net_weight_history['input2 weights'] = input2_weights.tolist()
 net_weight_history['connectivity matrix'] = np.asarray(connectivity_matrix).tolist()
 net_weight_history['output weights'] = np.asarray(output_weight_matrix).tolist()
 
