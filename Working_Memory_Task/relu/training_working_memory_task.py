1e5dd90697fd8296a9273805f970d574d6a529ba
Amrut Nadgir <31873854+amrutn@users.noreply.github.com>
2021-01-17 08:26:08 -0800

    changed connectivity matrix WM task

--- Working_Memory_Task/relu/training_working_memory_task.py
+++ Working_Memory_Task/relu/training_working_memory_task.py
@@ -21,7 +21,7 @@ connectivity_matrix = np.ones((num_nodes, num_nodes))
 weight_matrix = np.random.normal(0, 1/np.sqrt(num_nodes), (num_nodes, num_nodes))
 for i in range(num_nodes):
     weight_matrix[i,i] = 0
-    connectivity_matrix[i,i] = 0
+    #connectivity_matrix[i,i] = 0
 weight_matrix = tf.Variable(weight_matrix)
 connectivity_matrix = tf.constant(connectivity_matrix)
 
